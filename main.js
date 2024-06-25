05ad4c8035634079af465d87cc715c8dc56cf28b
aaravkarn0208 <bibek01.karn@gmail.com>
2024-06-25 13:19:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name="Ranbir";
+var last_name="Kapoor";
+console.log(first_name);
+console.log(last_name);
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var a=10;
+var b=5;
+var c=a+b;
+console.log(a);
+console.log(b);
+console.log("10 + 5 = "+c);
